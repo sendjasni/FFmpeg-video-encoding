a556a3e4560ed7ea46239a6bae381359f7ac8bab
sendja <sendjasni.93@gmail.com>
2017-04-19 12:28:42 +0200

    Create the encode module using ffmpeg and libx265

---
+++ ffmpeg_encode.py
@@ -0,0 +1,40 @@
+import os, subprocess
+
+# path to ffmpeg bin
+FFMPEG_PATH = '/usr/local/bin/ffmpeg'
+# params for video encoding
+WIDTH = 1080
+HEIGHT = 720
+FRAME_RATE = 30
+
+
+def process():
+    cwd = os.getcwd()
+
+    # get a list of files that have the extension y4m
+    filelist = filter(lambda f: f.split('.')[-1] == 'y4m', os.listdir(cwd))
+    filelist = sorted(filelist)
+
+    # encode each file
+    for file in filelist:
+        encode(file)
+
+
+def encode(file):
+    name = ''.join(file.split('.')[:-1])
+    output = '{}.h265'.format(name)
+
+    #try:
+    command = [FFMPEG_PATH, '-i', file, '-s', WIDTH, 'x', HEIGHT, '-r', FRAME_RATE, '-c:v libx265e -c:a copy',
+                   '-threads', '8', output]
+    subprocess.call(command)  # encode the video!
+    # except  subprocess.CalledProcessError as err:
+    #     et =   err.returncode
+    #     if ret in (1, 2):
+    #         print("the command failed")
+    #     elif ret in (3, 4, 5):
+    #         print("the command failed very much")
+
+
+if __name__ == "__main__":
+    process()
